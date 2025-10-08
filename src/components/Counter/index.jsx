acc8ae692eb963e9580a08d689d1b7a7c3f09ceb
Nadezhda Mikhonova <mihon.nadin@gmail.com>
2025-10-08 16:54:56 +0300

    lesson 43

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment, incrementByAmount } from "../../slices/counter/counterSlice";
+import { decrement, decrementAsync, increment, incrementAsync, incrementByAmount } from "../../slices/counter/counterSlice";
 import * as SC from './styles'
 
 export const Counter = () => {
@@ -8,16 +8,34 @@ export const Counter = () => {
     const dispatch = useDispatch();
 
     const [amount, setAmount] = useState();
+    const [asyncAmount, setAsyncAmount] = useState();
+    const [asyncDecAmount, setAsyncDecAmount] = useState();
 
     const handleAmount = () => {
         dispatch(incrementByAmount(amount));
         setAmount(null);
     };
 
+    const handleIncrementAsync = () => {
+        dispatch(incrementAsync(asyncAmount));
+        setAsyncAmount(null);
+    };
+
+        const handleDecrementAsync = () => {
+        dispatch(decrementAsync(asyncDecAmount));
+        setAsyncDecAmount(null);
+    };
+
     return (
         <div>
             <SC.Counter>
                 <SC.SimpleCounter>
+                    <button
+                        aria-label="Async add 2"
+                        onClick={() =>  dispatch(incrementAsync(2))}
+                    >
+                        Асинхронно добавить 2
+                    </button>
                     <button
                         aria-label="Increment value"
                         onClick={() =>  dispatch(increment())}
@@ -44,6 +62,34 @@ export const Counter = () => {
                         onClick={() => handleAmount()}
                     >
                         Прибавить
+                    </button>
+                </SC.AmountCounter>
+                <SC.AmountCounter>
+                    <input
+                        value={asyncAmount || ''}
+                        type="number"
+                        placeholder="Введите число"
+                        onChange={(e) => setAsyncAmount(Number(e.target.value))}
+                    />
+                    <button
+                        aria-label="Increment Async by amount value"
+                        onClick={() => handleIncrementAsync()}
+                    >
+                        Асинхронно прибавить
+                    </button>
+                </SC.AmountCounter>
+                <SC.AmountCounter>
+                    <input
+                        value={asyncDecAmount || ''}
+                        type="number"
+                        placeholder="Введите число"
+                        onChange={(e) => setAsyncDecAmount(Number(e.target.value))}
+                    />
+                    <button
+                        aria-label="Decrement Async by amount value"
+                        onClick={() => handleDecrementAsync()}
+                    >
+                        Асинхронно отнять
                     </button>
                 </SC.AmountCounter>
             </SC.Counter>
